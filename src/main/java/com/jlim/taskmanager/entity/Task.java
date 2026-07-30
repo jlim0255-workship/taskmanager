b166d0744b038c326db645e84510005da6a4537d
jlim0255-workship <jlim0255.work@gmail.com>
2026-07-30 15:04:06 +0800

    apply validation on create and update

--- src/main/java/com/jlim/taskmanager/entity/Task.java
+++ src/main/java/com/jlim/taskmanager/entity/Task.java
@@ -1,6 +1,8 @@
 package com.jlim.taskmanager.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,9 +20,12 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Title is mandatory")
+    @Size(min=3, max = 100, message = "Title must be between 3 and 100 characters")
     @Column(nullable = false)
     private String title;
 
+    @NotBlank(message = "Description is mandatory")
     @Column(length = 500)
     private String description;
 
