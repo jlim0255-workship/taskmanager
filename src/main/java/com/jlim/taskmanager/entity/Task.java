c479b2158593e5f4893656fdc1e6bc0288348b98
jlim0255-workship <jlim0255.work@gmail.com>
2026-07-30 11:37:49 +0800

    convert POJO into an actual entity in H2 db
    POJO = plain old java object, object that is not stricted int o any framework, just an object

--- src/main/java/com/jlim/taskmanager/entity/Task.java
+++ src/main/java/com/jlim/taskmanager/entity/Task.java
@@ -1,5 +1,6 @@
 package com.jlim.taskmanager.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,29 @@ import java.time.LocalDateTime;
 @Data // gives getters, setter, toString, equals and hash code method boiler plate
 @NoArgsConstructor // creates constructor with no args
 @AllArgsConstructor // apply these fields to the constructor
+@Entity
+@Table(name="tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(length = 500)
     private String description;
+
+    @Column(nullable = false)
     private Boolean completed;
+
+    @Column(name="created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    // protected method to set createdAt before persisting the entity
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
