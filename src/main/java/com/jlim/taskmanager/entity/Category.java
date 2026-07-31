45e8f66246b14ec378ab2c67dd0e33f2a2f80e19
jlim0255-workship <jlim0255.work@gmail.com>
2026-07-31 22:27:53 +0800

    new category entity

---
+++ src/main/java/com/jlim/taskmanager/entity/Category.java
@@ -0,0 +1,23 @@
+package com.jlim.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+}
